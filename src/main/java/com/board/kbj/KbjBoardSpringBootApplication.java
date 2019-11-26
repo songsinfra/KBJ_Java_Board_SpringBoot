96024dd1239369008fc240e15add6b5d39dd8b4d
DesktopKBJ <kbj0109@gmail.com>
2019-11-27 00:23:12 +0900

    프로젝트 첫 생성

---
+++ src/main/java/com/board/kbj/KbjBoardSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.board.kbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbjBoardSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbjBoardSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/kbj/KbjBoardSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.kbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbjBoardSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
